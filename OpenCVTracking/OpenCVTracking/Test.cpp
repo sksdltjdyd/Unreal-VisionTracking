a5f1f78976048bfa79cb830f2a5079418050c0fa
SUROM <62508307+sksdltjdyd@users.noreply.github.com>
2025-07-04 16:02:28 +0900

    d

--- OpenCVTracking/OpenCVTracking/Test.cpp
+++ OpenCVTracking/OpenCVTracking/Test.cpp
@@ -2,3 +2,15 @@
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
 #include <iostream>
+
+using namespace cv;
+using namespace std;
+
+///////// Images ////////
+
+void main() {
+	string path = "C:/Users/User\Documents/Git/Unreal-VisionTracking/Resources/Image1.jpg";
+	Mat img = imread(path);
+	imshow("Image", img);
+	waitKey(0);
+}
