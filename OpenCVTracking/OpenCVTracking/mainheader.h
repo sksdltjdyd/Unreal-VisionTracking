9dbb78356a08e8a23063c1534379376a096e28af
SUROM <62508307+sksdltjdyd@users.noreply.github.com>
2025-07-31 17:46:17 +0900

    Create mainheader.h

---
+++ OpenCVTracking/OpenCVTracking/mainheader.h
@@ -0,0 +1,14 @@
+#ifndef __MAIN_H__
+#define __MAIN_H__
+
+#include <iostream>
+#include <chrono>
+#include <thread>
+#include <deque>
+#include <memory>
+
+void on_trackbar(int, void*);
+
+
+#endif // !__MAIN_H__
+
